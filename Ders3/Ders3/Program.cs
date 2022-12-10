42d1afe29a1bc2588dbb6dd8fa3a82a533377af6
Mehmet Başgöze <92752077+memollizm@users.noreply.github.com>
2022-12-10 14:29:09 +0300

    Add files via upload

---
+++ Ders3/Ders3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1, sayi2, toplam;
+            Console.Write("Sayı 1: ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Sayı 2: ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            toplam = sayi1 + sayi2;
+
+            Console.WriteLine("Toplam = "+toplam);
+            Console.Read();
+        }
+    }
+}
